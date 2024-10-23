0c13f7d756700de43e32906e35833fdb0c2c2877
TheTinyCoder <charawe.yw@gmail.com>
2024-10-23 22:37:10 +0300

    resolve

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python3
 """
-Caching request module
+Module with tools for request caching and tracking using Redis.
 """
+
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
+# Initialize a Redis instance for caching and tracking
+redis_store = redis.Redis()
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
+
+def data_cacher(method: Callable) -> Callable:
+    """Decorator to cache the output of a function and track request count.
+
+    Args:
+        method (Callable): The function whose output is to be cached.
+
+    Returns:
+        Callable: The wrapped function with caching and tracking.
     """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
+    @wraps(method)
+    def invoker(url) -> str:
         """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
+        Caches the result of the URL fetch and tracks the request count.
+
+        Args:
+            url (str): The URL to fetch data from.
+
+        Returns:
+            str: The content of the URL, either from cache or fetched anew.
+        """
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
-@track_get_page
+@data_cacher
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
     """
-    response = requests.get(url)
-    return response.text
+    Fetches and returns the content of a URL, with caching.
+
+    Args:
+        url (str): The URL to fetch data from.
+
+    Returns:
+        str: The content of the URL.
+    """
+    return requests.get(url).text
